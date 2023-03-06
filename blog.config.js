e0a7db2267c0d2487ceb1597c2f3a2c8258155ae
YDDDKING <57852907+YDDDKING@users.noreply.github.com>
2023-03-07 00:28:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -11,7 +11,7 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '苏思醒', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '苏思醒的博客', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个平平淡淡的打工人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
